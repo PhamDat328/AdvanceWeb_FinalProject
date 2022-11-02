0d94260f4f4f6dcd44fb7f382d9827ccce1cc2b0
Phong <thanhphong07022002@gmail.com>
2022-11-02 10:28:42 +0700

    2/11/2022 ong bau

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,43 @@
 const User = require("../models/User");
+const formidable = require("formidable");
 // const bcrypt = require("bcrypt");
 // const salt = await bcrypt.genSalt(10);
 // const hash = await bcrypt.hash(password, salt);
+
+module.exports = {
+
+    getHomePage: (req, res, next) => {
+
+        res.render("index", { title: "SmartWallet" })
+
+    },
+
+    getDepositForm: (req, res, next) => {
+
+        res.render("Deposit", { title: "SmartWallet" })
+
+    },
+
+    getWithdrawForm: (req, res, next) => {
+
+        res.render("Withdraw", { title: "SmartWallet" })
+
+    },
+
+    getTransferForm: (req, res, next) => {
+
+        res.render("Transfer", { title: "SmartWallet" })
+
+    },
+
+    getBuyPhoneCardForm: (req, res, next) => {
+
+        res.render("BuyPhoneCard", { title: "SmartWallet" })
+
+    },
+    getTransactionHistory: (req, res, next) => {
+
+        res.render("index", { title: "SmartWallet" })
+
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const userController = require("../controllers/userController")
 const User = require("../models/User");
 
 /* GET home page. */
 
 User.find((err, users) => {
-  if (users.length) return;
+    if (users.length) return;
 
-  new User({
-    fullName: "dat_pham",
-    password: "abc123",
-    address: "VietNam",
-    email: "dat09@gmail.com",
-  }).save();
+    new User({
+        fullName: "dat_pham",
+        password: "abc123",
+        address: "VietNam",
+        email: "dat09@gmail.com",
+    }).save();
 });
 
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express", layout: "main" });
-});
-router.get("/pending", function (req, res, next) {
-  res.render("pending", { title: "Express", layout: "main" });
-});
+
+router.route("/").get(userController.getHomePage)
 
 router.route("/login").get(authController.getLoginPage);
-router
-  .route("/register")
-  .get(authController.getRegisterPage)
-  .post(authController.postRegisterPage);
+
+router.route("/register").get(authController.getRegisterPage).post(authController.postRegisterPage);
 
 module.exports = router;
